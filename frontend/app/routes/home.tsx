a58b7d34ca7d710a4cac4daa97ba89dbaaaef477
Sergei Tishkov <tishkovsergei92@gmail.com>
2025-06-07 16:40:15 +0300

    home page title is added

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../components/welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Give Me Tokens Please!!!" },
+    { name: "description", content: "Gi-Me-To-Ple!!! home page" },
   ];
 }
 
