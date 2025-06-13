c764238e796c5156f4a0d33f380a9871e614c5b1
Sergei Tishkov <tishkovsergei92@gmail.com>
2025-06-13 19:50:43 +0300

    autoformatted

--- frontend/src/app/HomePage.tsx
+++ frontend/src/app/HomePage.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { Menu, Search } from "lucide-react";
 import { CrowdfundingRequest } from "@gi-me-to-ple/shared/types/CrowdfundingRequest";
 
 type HomePageProps = {
-  requests: CrowdfundingRequest[]
-}
+  requests: CrowdfundingRequest[];
+};
 
 export function HomePage({ requests }: HomePageProps) {
   const [menuOpen, setMenuOpen] = useState(false);
